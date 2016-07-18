0d9ab795fb800c96f2c0d24cb0b3d42f3672e7ab
Tanuj Jain <tanujjain244@yahoo.com>
2016-07-18 09:49:17 +0530

    Update and rename aospb.mk to atomic.mk

--- atomic.mk
+++ atomic.mk
@@ -3,18 +3,18 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 
-# Inherit some common AOSPB stuff.
-$(call inherit-product, vendor/aospb/config/common_full_phone.mk)
+# Inherit some common Atomic stuff.
+$(call inherit-product, vendor/atomic/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/aospb/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/atomic/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hammerhead
-PRODUCT_NAME := aospb_hammerhead
+PRODUCT_NAME := atomic_hammerhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
